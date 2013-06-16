69d5f240bbd61c4b84d217ef666405c5e5da540b
Jeremy Sellars <jeremy.sellars@softekinc.com>
2013-06-16 16:04:48 -0500

    Moved MatchLength to own file.

--- HighRegex/IExpression.cs
+++ HighRegex/IExpression.cs
@@ -3,34 +3,6 @@ using System.Collections.Generic;
 
 namespace HighRegex
 {
-   public struct MatchLength
-   {
-      public static readonly MatchLength NoMatch = new MatchLength();
-
-      public static MatchLength Of(int length)
-      {
-         return new MatchLength(length);
-      }
-
-      public static MatchLength EmptyMatch(bool success)
-      {
-         return new MatchLength(0, success);
-      }
-
-      public static MatchLength ClassMatchIf(bool success)
-      {
-         return new MatchLength(success ? 1 : 0, success);
-      }
-
-      public MatchLength(int length, bool success = true)
-      {
-         Length = length;
-         Success = success;
-      }
-
-      public readonly bool Success;
-      public readonly int Length;
-   }
    /// <summary>
    /// Represents a regular expression capable of matching input at a specific index.
    /// </summary>
