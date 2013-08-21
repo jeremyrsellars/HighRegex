70622910fe567890700a8c7ae488888c5ecec484
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-08-20 23:18:53 -0500

    Added basic RegexTokinzer and tests

---
+++ HighRegex.UnitTest/ParsedExpressionSupport/RegexTokenizerTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HighRegex.ParsedExpressionSupport
+{
+   [TestClass]
+   public abstract class RegexTokenizerTest
+   {
+      protected abstract string[] ExpectedTokens { get; }
+      protected RegexTokenizer tokenizer;
+
+      [TestInitialize]
+      public virtual void Init()
+      {
+         tokenizer = new RegexTokenizer(GetRegex());
+      }
+
+      [TestMethod]
+      public void TokensAreAsExpected()
+      {
+         var tokens = tokenizer.Tokenize(GetTokenString()).ToList();
+         CollectionAssert.AreEqual(ExpectedTokens, tokens);
+      }
+
+      protected abstract string GetTokenString();
+
+      protected abstract Regex GetRegex();
+
+      public abstract class SingleCharacterRegexTokenizerTest : RegexTokenizerTest
+      {
+         protected override Regex GetRegex()
+         {
+            return new Regex(".");
+         }
+         protected override string[] ExpectedTokens
+         {
+            get { return GetTokenString().Select(c => c.ToString()).ToArray(); }
+         }
+      }
+
+      [TestClass]
+      public class WhenTokenizingEmptyString : SingleCharacterRegexTokenizerTest
+      {
+         protected override string GetTokenString()
+         {
+            return "";
+         }
+      }
+
+      [TestClass]
+      public class WhenTokenizingSingleCharacterString : SingleCharacterRegexTokenizerTest
+      {
+         protected override string GetTokenString()
+         {
+            return "a";
+         }
+      }
+
+      [TestClass]
+      public class WhenTokenizingSeveralCharacters : SingleCharacterRegexTokenizerTest
+      {
+         protected override string GetTokenString()
+         {
+            return "aBcDeF";
+         }
+      }
+   }
+}
---
+++ HighRegex/ParsedExpressionSupport/RegexTokenizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HighRegex.ParsedExpressionSupport
+{
+   public class RegexTokenizer
+   {
+      readonly Regex regex;
+
+      public RegexTokenizer(Regex regex)
+      {
+         this.regex = regex;
+      }
+
+      public IEnumerable<string> Tokenize(string s)
+      {
+         for(var m = regex.Match(s); m.Success; m = m.NextMatch())
+            yield return m.Value;
+      }
+   }
+}
